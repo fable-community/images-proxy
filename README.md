1bf37fbb254079d3daaf5c0a5560fd7ef0479437
Kerolos Zaki <ker0olos@pm.me>
2023-05-16 08:22:28 +0000

    fetch image and then respond with it

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # images-proxy
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/fable-community/images-proxy)
+
+```sh
+npm run dev
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,106 @@
 use worker::*;
 
+use std::collections::HashMap;
+
+use anyhow::Context;
+
+use image::{DynamicImage, GenericImageView};
+
+enum ImageSize {
+    Preview,   // 64x64
+    Thumbnail, // 110x155
+    Medium,    // 230x325
+    Large,     // 450x635,
+}
+
+async fn fetch_image(req: &Request) -> anyhow::Result<image::DynamicImage> {
+    let url = urlencoding::decode(&req.path()[1..])
+        .context("failed to decode url")?
+        .to_string();
+
+    let response = reqwest::get(url).await.context("failed to fetch image")?;
+
+    let image_data = response
+        .bytes()
+        .await
+        .context("failed to get image bytes")?;
+
+    let image = image::load_from_memory(&image_data).context("failed to decode image")?;
+
+    Ok(image)
+}
+
+fn resize_image_cover(
+    image: DynamicImage,
+    desired_width: u32,
+    desired_height: u32,
+) -> DynamicImage {
+    // TODO
+
+    image
+}
+
+async fn fetch_image_resize(req: &Request, size: ImageSize) -> anyhow::Result<image::RgbaImage> {
+    let image = fetch_image(&req).await?;
+
+    let (width, height) = match size {
+        ImageSize::Preview => (64, 64),
+        ImageSize::Thumbnail => (110, 155),
+        ImageSize::Medium => (230, 325),
+        ImageSize::Large => (450, 635),
+    };
+
+    let resized_image = resize_image_cover(image, width, height);
+
+    Ok(resized_image.into_rgba8())
+}
+
+fn respond_with_image(image: image::RgbaImage) -> Response {
+    let mut buf = std::io::Cursor::new(Vec::new());
+
+    let mut headers = Headers::new();
+
+    image
+        .write_to(&mut buf, image::ImageOutputFormat::Png)
+        .unwrap();
+
+    let data = buf.get_ref().clone();
+
+    headers.set("content-type", "image/png").unwrap();
+
+    headers
+        .set("content-length", &data.len().to_string())
+        .unwrap();
+
+    Response::from_body(ResponseBody::Body(data))
+        .unwrap()
+        .with_headers(headers)
+}
+
 #[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
+    let hash_query = req
+        .url()?
+        .query_pairs()
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+
+    let _blur = hash_query.contains_key("blur");
+
+    let size = match hash_query.get("size").and_then(|x| Some(x.as_str())) {
+        Some("preview") => ImageSize::Preview,
+        Some("thumbnail") => ImageSize::Thumbnail,
+        Some("medium") => ImageSize::Medium,
+        _ => ImageSize::Large,
+    };
+
+    match fetch_image_resize(&req, size).await {
+        Ok(image) => Ok(respond_with_image(image)),
+        Err(_) => {
+            // TODO
+            let image = image::RgbaImage::new(256, 256);
+
+            Ok(respond_with_image(image))
+        }
+    }
 }
