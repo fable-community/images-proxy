9aab1240ec8b2692fb42f5bba0f072dcfb2b3648
Kerolos Zaki <ker0olos@pm.me>
2023-05-17 18:59:23 +0300

    Migrate to Deno Deploy (#2)
    * fix negative height
    
    * use crate `fastblur` to lower cpu time usage
    
    * use crate `fast_image_resize` to lower cpu time usage
    
    * migrate the project to deno deploy using wasm-pack
    
    * utilize cache as long as the server is alive
    
    * keep resized jpegs as jpeg

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 [![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/fable-community/images-proxy)
 
 ```sh
-npm run dev
+deno task start
 ```
 
-This service is used in [Fable](https://github.com/ker0olos/fable) to resize all characters and media images
+This service is used in [Fable](https://github.com/ker0olos/fable) to resize all
+characters and media images
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use worker::*;
-
-use std::collections::HashMap;
-
 use anyhow::Context;
-
-use image::{DynamicImage, GenericImageView, RgbaImage};
+use fast_image_resize as fr;
+use fastblur::gaussian_blur;
+use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbaImage};
+use std::{collections::HashMap, num::NonZeroU32, str::FromStr};
+use url::Url;
+use wasm_bindgen::prelude::*;
+use web_sys::{Headers, Request, Response, ResponseInit};
 
 #[derive(Debug)]
 enum ImageSize {
@@ -14,40 +15,69 @@ enum ImageSize {
     Large,     // 450x635,
 }
 
-async fn fetch_image(req: &Request) -> anyhow::Result<image::DynamicImage> {
-    let url = urlencoding::decode(&req.path()[1..])
-        .context("failed to decode url")?
+#[derive(Debug)]
+enum ImageFormat {
+    Png,
+    Jpeg,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+async fn fetch_image(url: &Url) -> anyhow::Result<(DynamicImage, ImageFormat)> {
+    let image_url = urlencoding::decode(&url.path()[1..])
+        .context(format!("failed to decode url {}", url.path()))?
         .to_string();
 
-    // TODO cache and load from cache
-
-    let response = reqwest::get(url).await.context("failed to fetch image")?;
+    let response = reqwest::get(image_url)
+        .await
+        .context(format!("failed to fetch {}", url.path()))?;
 
     let content_type = response
         .headers()
         .get("Content-Type")
         .and_then(|x| x.to_str().ok());
 
-    console_log!("{}, {:?}", response.status(), content_type);
+    let format = match content_type {
+        Some("image/jpeg") => ImageFormat::Jpeg,
+        // webp encoding is not supported while targting wasm
+        // all webp images will be convented to png
+        Some("image/png") | Some("image/webp") => ImageFormat::Png,
+        Some(s) => return Err(anyhow::anyhow!(format!("image type {} no allowed", s))),
+        None => return Err(anyhow::anyhow!("No content-type header")),
+    };
 
-    match content_type {
-        Some("image/png") => (),
-        Some("image/jpeg") => (),
-        Some("image/webp") => (),
-        _ => return Err(anyhow::anyhow!("image type no allowed")),
-    }
+    console_log!(
+        "{}, {:?}, {:?}, {}",
+        response.status(),
+        content_type,
+        format,
+        url.path()
+    );
 
     let image_data = response
         .bytes()
         .await
-        .context("failed to get image bytes")?;
+        .context("failed to convent response body to bytes")?;
 
-    let image = image::load_from_memory(&image_data).context("failed to decode image")?;
+    let img =
+        image::load_from_memory(&image_data).context("failed to laod image from response body")?;
 
-    Ok(image)
+    Ok((img, format))
 }
 
-fn resize_fit_cover(image: DynamicImage, desired_width: u32, desired_height: u32) -> RgbaImage {
+fn resize_fit_cover(
+    image: DynamicImage,
+    desired_width: u32,
+    desired_height: u32,
+) -> anyhow::Result<RgbaImage> {
     let (width, height) = image.dimensions();
 
     let scale_factor = if desired_width / desired_height > width / height {
@@ -56,35 +86,65 @@ fn resize_fit_cover(image: DynamicImage, desired_width: u32, desired_height: u32
         desired_height as f32 / height as f32
     };
 
-    let new_width = (width as f32 * scale_factor) as u32;
-    let new_height = (height as f32 * scale_factor) as u32;
+    let new_width = std::cmp::max((width as f32 * scale_factor) as u32, desired_width);
+    let new_height = std::cmp::max((height as f32 * scale_factor) as u32, desired_height);
 
     let crop_x = (new_width - desired_width) / 2;
     let crop_y = (new_height - desired_height) / 2;
 
-    let mut resized_img = image::imageops::resize(
-        &image,
-        new_width,
-        new_height,
-        image::imageops::FilterType::Lanczos3,
+    // console_log!(
+    //     "{}, {}, {}, {}",
+    //     new_width,
+    //     new_height,
+    //     new_width as i32 - desired_width as i32,
+    //     new_height as i32 - desired_height as i32
+    // );
+
+    let mut src_image = fr::Image::from_vec_u8(
+        NonZeroU32::new(width).unwrap(),
+        NonZeroU32::new(height).unwrap(),
+        image.to_rgba8().into_raw(),
+        fr::PixelType::U8x4,
+    )?;
+
+    let alpha_mul_div = fr::MulDiv::default();
+
+    alpha_mul_div.multiply_alpha_inplace(&mut src_image.view_mut())?;
+
+    let mut dst_image = fr::Image::new(
+        NonZeroU32::new(new_width).unwrap(),
+        NonZeroU32::new(new_height).unwrap(),
+        src_image.pixel_type(),
     );
 
-    image::imageops::crop(
+    let mut dst_view = dst_image.view_mut();
+
+    let mut resizer = fr::Resizer::new(fr::ResizeAlg::Convolution(fr::FilterType::Hamming));
+
+    resizer.resize(&src_image.view(), &mut dst_view)?;
+
+    alpha_mul_div.divide_alpha_inplace(&mut dst_view)?;
+
+    let buffer = RgbaImage::from_raw(new_width, new_height, dst_image.buffer().to_vec()).unwrap();
+
+    let mut resized_img = DynamicImage::ImageRgba8(buffer);
+
+    Ok(image::imageops::crop(
         &mut resized_img,
         crop_x,
         crop_y,
         desired_width,
         desired_height,
     )
-    .to_image()
+    .to_image())
 }
 
 async fn fetch_image_resize(
-    req: &Request,
+    url: &Url,
     size: &ImageSize,
     blur: bool,
-) -> anyhow::Result<image::RgbaImage> {
-    let image = fetch_image(&req).await?;
+) -> anyhow::Result<(DynamicImage, ImageFormat)> {
+    let (img, format) = fetch_image(url).await?;
 
     let (width, height) = match size {
         ImageSize::Preview => (64, 64),
@@ -93,53 +153,96 @@ async fn fetch_image_resize(
         ImageSize::Large => (450, 635),
     };
 
-    let resized_image = resize_fit_cover(image, width, height);
+    let img = DynamicImage::ImageRgba8(
+        resize_fit_cover(img, width, height).context("failed to resize image")?,
+    );
 
     if blur {
-        Ok(image::imageops::blur(&resized_image, 15.0))
+        let (width, height) = img.dimensions();
+
+        let mut data: Vec<[u8; 3]> = img
+            .pixels()
+            .map(|(_, _, pixel)| {
+                let [r, g, b, _] = pixel.0;
+                [r, g, b]
+            })
+            .collect();
+
+        gaussian_blur(&mut data, width as usize, height as usize, 10.0);
+
+        let blurred: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_fn(width, height, |x, y| {
+            let pixel = data[(y * width + x) as usize];
+            Rgb([pixel[0], pixel[1], pixel[2]])
+        });
+
+        Ok((DynamicImage::ImageRgb8(blurred), ImageFormat::Jpeg))
     } else {
-        Ok(resized_image)
+        Ok((img, format))
     }
 }
 
-fn respond_with_image(image: image::RgbaImage) -> anyhow::Result<Response> {
+fn respond_with_image(image: DynamicImage, format: ImageFormat) -> anyhow::Result<Response> {
+    let headers = match Headers::new() {
+        Ok(x) => x,
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
+    };
+
     let mut buf = std::io::Cursor::new(Vec::new());
 
-    let mut headers = Headers::new();
-
     image
-        .write_to(&mut buf, image::ImageOutputFormat::Png)
-        .context("failed to encode image to png")?;
+        .write_to(
+            &mut buf,
+            match format {
+                ImageFormat::Png => image::ImageOutputFormat::Png,
+                ImageFormat::Jpeg => image::ImageOutputFormat::Jpeg(80),
+            },
+        )
+        .context("failed to encode resized image")?;
 
-    let data = buf.get_ref().clone();
+    let mut data = buf.get_ref().clone();
 
-    match headers.set("content-type", "image/png") {
+    match headers.set(
+        "content-type",
+        match format {
+            ImageFormat::Png => "image/png",
+            ImageFormat::Jpeg => "image/jpeg",
+        },
+    ) {
         Ok(_) => (),
-        Err(_) => return Err(anyhow::anyhow!("failed to write content-type header")),
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
     };
 
     match headers.set("content-length", &data.len().to_string()) {
         Ok(_) => (),
-        Err(_) => return Err(anyhow::anyhow!("failed to write content-length header")),
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
     };
 
-    match Response::from_body(ResponseBody::Body(data)) {
-        Ok(response) => Ok(response.with_headers(headers)),
-        Err(_) => Err(anyhow::anyhow!("failed to write response body")),
-    }
+    let response = match Response::new_with_opt_u8_array_and_init(Some(&mut data), &{
+        let mut init = ResponseInit::new();
+        init.headers(&headers);
+        init
+    }) {
+        Ok(x) => x,
+        Err(_) => return Err(anyhow::anyhow!("failed to create new response object")),
+    };
+
+    Ok(response)
 }
 
-#[event(fetch)]
-async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
-    let hash_query = req
-        .url()?
+#[wasm_bindgen]
+pub async fn handler(request: Request) -> Response {
+    console_error_panic_hook::set_once();
+
+    let url = Url::from_str(&request.url()).unwrap();
+
+    let hash_query = url
         .query_pairs()
         .into_owned()
         .collect::<HashMap<String, String>>();
 
     let blur = hash_query.contains_key("blur");
 
-    let size = match hash_query.get("size").and_then(|x| Some(x.as_str())) {
+    let size = match hash_query.get("size").map(|x| x.as_str()) {
         Some("preview") => ImageSize::Preview,
         Some("thumbnail") => ImageSize::Thumbnail,
         Some("medium") => ImageSize::Medium,
@@ -148,9 +251,11 @@ async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response
 
     // console_log!("size: {:?}, blur: {}", size, blur);
 
-    match fetch_image_resize(&req, &size, blur).await {
-        Ok(image) => Ok(respond_with_image(image).unwrap()),
-        Err(_) => {
+    match fetch_image_resize(&url, &size, blur).await {
+        Ok((img, f)) => respond_with_image(img, f).unwrap(),
+        Err(_err) => {
+            // console_log!("{:?}", _err);
+
             let default_image: &[u8] = match &size {
                 ImageSize::Preview | ImageSize::Thumbnail => {
                     include_bytes!("../default/thumbnail.png")
@@ -158,9 +263,11 @@ async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Response
                 _ => include_bytes!("../default/medium.png"),
             };
 
-            let image = image::load_from_memory(default_image).unwrap().to_rgba8();
-
-            Ok(respond_with_image(image).unwrap())
+            respond_with_image(
+                image::load_from_memory(default_image).unwrap(),
+                ImageFormat::Png,
+            )
+            .unwrap()
         }
     }
 }
