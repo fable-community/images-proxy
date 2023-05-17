a091bf547d8f216056a63f9ab1fafd827e9e403d
Kerolos Zaki <ker0olos@pm.me>
2023-05-17 11:01:27 +0000

    utilize cache as long as the server is alive

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use anyhow::Context;
 use fast_image_resize as fr;
 use fastblur::gaussian_blur;
 use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb, RgbaImage};
 use std::{collections::HashMap, num::NonZeroU32, str::FromStr};
-use url::{ParseError, Url};
+use url::Url;
 use wasm_bindgen::prelude::*;
-use web_sys::{Headers, Request, Response};
+use web_sys::{Headers, Request, Response, ResponseInit};
 
 #[derive(Debug)]
 enum ImageSize {
@@ -25,27 +26,36 @@ macro_rules! console_log {
 }
 
 async fn fetch_image(url: &Url) -> anyhow::Result<DynamicImage> {
-    let image_url = urlencoding::decode(&url.path()[1..])?.to_string();
+    let image_url = urlencoding::decode(&url.path()[1..])
+        .context(format!("failed to decode url {}", url.path()))?
+        .to_string();
 
-    let response = reqwest::get(image_url).await?;
+    let response = reqwest::get(image_url)
+        .await
+        .context(format!("failed to fetch {}", url.path()))?;
 
     let content_type = response
         .headers()
         .get("Content-Type")
         .and_then(|x| x.to_str().ok());
 
-    console_log!("{}, {:?}", response.status(), content_type);
+    console_log!("{}, {:?}, {}", response.status(), content_type, url.path());
 
     match content_type {
         Some("image/png") => (),
         Some("image/jpeg") => (),
         Some("image/webp") => (),
-        _ => return Err(anyhow::anyhow!("image type no allowed")),
+        Some(s) => return Err(anyhow::anyhow!(format!("image type {} no allowed", s))),
+        None => return Err(anyhow::anyhow!("No content-type header")),
     }
 
-    let image_data = response.bytes().await?;
+    let image_data = response
+        .bytes()
+        .await
+        .context("failed to convent response body to bytes")?;
 
-    let image = image::load_from_memory(&image_data)?;
+    let image =
+        image::load_from_memory(&image_data).context("failed to laod image from response body")?;
 
     Ok(image)
 }
@@ -69,13 +79,13 @@ fn resize_fit_cover(
     let crop_x = (new_width - desired_width) / 2;
     let crop_y = (new_height - desired_height) / 2;
 
-    console_log!(
-        "{}, {}, {}, {}",
-        new_width,
-        new_height,
-        new_width as i32 - desired_width as i32,
-        new_height as i32 - desired_height as i32
-    );
+    // console_log!(
+    //     "{}, {}, {}, {}",
+    //     new_width,
+    //     new_height,
+    //     new_width as i32 - desired_width as i32,
+    //     new_height as i32 - desired_height as i32
+    // );
 
     let mut src_image = fr::Image::from_vec_u8(
         NonZeroU32::new(width).unwrap(),
@@ -131,7 +141,9 @@ async fn fetch_image_resize(
         ImageSize::Large => (450, 635),
     };
 
-    let img = DynamicImage::ImageRgba8(resize_fit_cover(image, width, height)?);
+    let img = DynamicImage::ImageRgba8(
+        resize_fit_cover(image, width, height).context("failed to resize image")?,
+    );
 
     if blur {
         let (width, height) = img.dimensions();
@@ -161,37 +173,34 @@ async fn fetch_image_resize(
 fn respond_with_image(image: DynamicImage) -> anyhow::Result<Response> {
     let headers = match Headers::new() {
         Ok(x) => x,
-        Err(_) => return Err(anyhow::anyhow!("")),
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
     };
 
     let mut buf = std::io::Cursor::new(Vec::new());
 
-    image.write_to(&mut buf, image::ImageOutputFormat::Png)?;
+    image
+        .write_to(&mut buf, image::ImageOutputFormat::Png)
+        .context("failed to encode resized image")?;
 
     let mut data = buf.get_ref().clone();
 
     match headers.set("content-type", "image/png") {
         Ok(_) => (),
-        Err(_) => return Err(anyhow::anyhow!("")),
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
     };
 
     match headers.set("content-length", &data.len().to_string()) {
         Ok(_) => (),
-        Err(_) => return Err(anyhow::anyhow!("")),
-    };
-
-    let headers = match Headers::new() {
-        Ok(x) => x,
-        Err(_) => return Err(anyhow::anyhow!("")),
+        Err(_) => return Err(anyhow::anyhow!("failed to set headers")),
     };
 
     let response = match Response::new_with_opt_u8_array_and_init(Some(&mut data), &{
-        let mut init = web_sys::ResponseInit::new();
+        let mut init = ResponseInit::new();
         init.headers(&headers);
         init
     }) {
         Ok(x) => x,
-        Err(_) => return Err(anyhow::anyhow!("")),
+        Err(_) => return Err(anyhow::anyhow!("failed to create new response object")),
     };
 
     Ok(response)
@@ -217,15 +226,13 @@ pub async fn handler(request: Request) -> Response {
         _ => ImageSize::Large,
     };
 
-    console_log!("size: {:?}, blur: {}", size, blur);
-
-    // let response = Response::new_with_opt_str(Some("hello")).unwrap();
-
-    // response
+    // console_log!("size: {:?}, blur: {}", size, blur);
 
     match fetch_image_resize(&url, &size, blur).await {
         Ok(image) => respond_with_image(image).unwrap(),
-        Err(_) => {
+        Err(_err) => {
+            // console_log!("{:?}", _err);
+
             let default_image: &[u8] = match &size {
                 ImageSize::Preview | ImageSize::Thumbnail => {
                     include_bytes!("../default/thumbnail.png")
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,51 @@
 import { serve } from 'https://deno.land/std@0.187.0/http/server.ts';
 
+import { LRU } from 'https://deno.land/x/lru@1.0.2/mod.ts';
+
 import { handler } from '../build/images_proxy.js';
 
-serve(handler);
+const TEN_MIB = 1024 * 1024 * 10;
+
+const lru = new LRU<{
+  body: ArrayBuffer;
+  headers: Headers;
+}>(20);
+
+serve(async (request) => {
+  const url = new URL(request.url);
+
+  const key = (url.pathname + url.search).substring(1);
+
+  if (key === 'favicon.ico') {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  const hit = lru.get(key);
+
+  if (hit) {
+    console.log(`cache hit: ${key}`);
+
+    return new Response(hit.body, {
+      headers: hit.headers,
+    });
+  }
+
+  const response = await handler(request);
+
+  if (Number(response.headers.get('content-length')) <= TEN_MIB) {
+    const v = {
+      body: await response.arrayBuffer(),
+      headers: response.headers,
+    };
+
+    lru.set(key, v);
+
+    return new Response(v.body, {
+      headers: v.headers,
+    });
+  } else {
+    return response;
+  }
+});
